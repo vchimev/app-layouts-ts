c76a50b55a196e35a36c3d4735126807396ae5bf
Vasil Chimev <vasil.chimev@gmail.com>
2019-05-08 05:04:43 -0400

    refactor: stack page

--- app/stack/stack-page.ts
+++ app/stack/stack-page.ts
@@ -1,6 +1,7 @@
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { EventData } from "tns-core-modules/data/observable";
+import { View } from "tns-core-modules/ui/core/view";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
 
 import { HomeViewModel } from "./stack-view-model";
@@ -8,9 +9,23 @@ import { HomeViewModel } from "./stack-view-model";
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
     page.bindingContext = new HomeViewModel();
+
+    // Animations
+    page.getViewById("container").eachChild(child => {
+        child.className = "btn";
+        child.className = "btn-animated";
+        return true;
+    });
 }
 
 export function onDrawerButtonTap(args: EventData) {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
+}
+
+export function onTap(args: EventData) {
+    let button = <View>args.object;
+    let className = button.className.replace("-animated", "").replace("2", "");
+    button.className = className;
+    button.className = className + "-animated2";
 }
