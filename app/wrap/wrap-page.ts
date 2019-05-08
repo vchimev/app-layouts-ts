472854091d239538b9165108721fba712b137c09
Vasil Chimev <vasil.chimev@gmail.com>
2019-05-08 05:04:58 -0400

    refactor: wrap page

--- app/wrap/wrap-page.ts
+++ app/wrap/wrap-page.ts
@@ -2,15 +2,30 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { EventData } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
+import { View } from "tns-core-modules/ui/core/view";
 
 import { HomeViewModel } from "./wrap-view-model";
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
     page.bindingContext = new HomeViewModel();
+
+    // Animations
+    page.getViewById("container").eachChild(child => {
+        child.className = "btn";
+        child.className = "btn-animated";
+        return true;
+    });
 }
 
 export function onDrawerButtonTap(args: EventData) {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
+}
+
+export function onTap(args: EventData) {
+    let button = <View>args.object;
+    let className = button.className.replace("-animated", "").replace("2", "");
+    button.className = className;
+    button.className = className + "-animated2";
 }
